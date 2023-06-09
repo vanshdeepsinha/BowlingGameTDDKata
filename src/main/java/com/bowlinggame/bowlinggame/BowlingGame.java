64f3bf6b634c6ad94584041f088681ec6298aa90
vanshdeep <vanshdeepsinha@gmail.com>
2023-06-09 17:06:41 +0530

    Intial project setup

---
+++ src/main/java/com/bowlinggame/bowlinggame/BowlingGame.java
@@ -0,0 +1,12 @@
+package com.bowlinggame.bowlinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingGame {
+
+	public static void main(String [] args) {
+		SpringApplication.run(BowlingGame.class, args);
+	}
+}
