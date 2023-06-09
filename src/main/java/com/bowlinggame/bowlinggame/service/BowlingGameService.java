46d74252915defdb2911c42b5e26f76c437bcaa5
vanshdeep <vanshdeepsinha@gmail.com>
2023-06-09 19:35:19 +0530

    feat: Add logic when a spare is scored on initall frame

--- src/main/java/com/bowlinggame/bowlinggame/service/BowlingGameService.java
+++ src/main/java/com/bowlinggame/bowlinggame/service/BowlingGameService.java
@@ -12,13 +12,22 @@ public class BowlingGameService {
 	public int evalGameScore(int [] eachGame) {
 		rolls = eachGame;
 		for(int frame=0, roll=0; frame <10; frame++, roll++) {
-			sumTurns(roll);
+			if(isSpare(roll)) {
+				gameScore += 10 + rolls[roll+2];
+				roll++;
+			} else {
+				sumTurns(roll);
+			}
 		}
 		return gameScore;
 	}
 	
 	private void sumTurns(int roll) {
 		gameScore += rolls[roll] + rolls[roll+1];
+	}
+	
+	private boolean isSpare(int roll) {
+		return rolls[roll] + rolls[roll+1] == 10;
 	}
 
 }
